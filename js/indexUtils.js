285467f2faea03ad42d2fa7267f97f20d6e2430d
parveenstk <parveen.kumar@suretekinfosoft.com>
2025-06-28 15:16:17 +0530

    working done.

---
+++ js/indexUtils.js
@@ -0,0 +1,40 @@
+// onClick subscrribe button 
+const packs = document.getElementById('packs');
+const subsBtn = document.querySelectorAll('.subscribe-button');
+const readytoyomz = document.getElementById('ready-to-yomz');
+
+subsBtn.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        replaceCls('ready-to-yomz', 'show', 'hide');
+        replaceCls('packs', 'hide', 'show');
+        packs.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+// utility function for multi uses
+const replaceCls = (element, existedClass, swichtedClass) => {
+    const el = document.getElementById(element);
+    el.classList.replace(existedClass, swichtedClass)
+};
+
+// tick points functionality
+const tickPoints = document.getElementById('tick-points');
+const img = './images/Tick-01.png';
+const text = [
+    "Choose how many bags",
+    "Pick YOMZ Original or Sour",
+    "Protect against Hidden Hunger",
+    "Try risk-free with our Picky Momz Happiness Guarantee",
+];
+
+const textHTML =
+    text.map(text => `
+        <div class="tick-point">
+          <img src="${img}" alt="tick-img" class="tick-image">
+          <p>${text}</p>
+        </div>
+      `)
+        .join('');
+
+tickPoints.innerHTML = textHTML;
