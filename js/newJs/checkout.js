e1b31ed809ea608cb4672cf09572c2bacfd0f511
parveenstk <parveen.kumar@suretekinfosoft.com>
2025-07-01 17:28:26 +0530

    everything is don, just some part are lfet.

--- js/newJs/checkout.js
+++ js/newJs/checkout.js
@@ -26,12 +26,18 @@ const additionalProducts = [
 // Updated Additional product in hmtl
 const updateAdditionalHTML = () => {
     const cartProducts = JSON.parse(localStorage.getItem('cartData'));
+    // console.log("cartProducts:", cartProducts);
 
+    // Additional Products
     const parent = document.getElementById('additionalProducts');
+    const parent2 = document.getElementById('additionalProducts2');
     let finalHTML = '';
+    let finalHTML2 = '';
     additionalProducts.forEach(product => {
         // Check if it exist in cart
         const existing = cartProducts.find(p => p.id === product.id);
+
+        // Desktops Products
         if (existing) return;
         const html = `
             <div id="${product.id}" class="cart-box-bottom">
@@ -40,7 +46,30 @@ const updateAdditionalHTML = () => {
                         <img src="${product.image}">
                     </div>
                     <div class="items-price">
-                        <div class="prdt-name">${product.title}<span>(${product.description})</span></div>
+                        <div class="prdt-name">${product.title}
+                            <span>${product.description}</span>
+                        </div>
+                        <div class="prdt-price">$${product.price}</div>
+                    </div>
+                </div>
+                <div class="text-center items-btn">
+                    <button type="button" class="btn btn-primary" onClick='addProduct("${product.id}")'>ADD</button>
+                </div>
+            </div>
+        `
+
+        // Mobile Products
+        if (existing) return;
+        const html2 = `
+            <div id="${product.id}" class="cart-box-bottom">
+                <div class="items">
+                    <div class="text-center items-image">
+                        <img src="${product.image}">
+                    </div>
+                    <div class="items-price">
+                        <div class="prdt-name">${product.title}
+                            <span>${product.description}</span>
+                        </div>
                         <div class="prdt-price">$${product.price}</div>
                     </div>
                 </div>
@@ -51,9 +80,11 @@ const updateAdditionalHTML = () => {
         `
 
         finalHTML += html;
+        finalHTML2 += html2;
     });
 
     parent && (parent.innerHTML = finalHTML);
+    parent2 && (parent2.innerHTML = finalHTML2);
 };
 
 // Get the selected pack from localStorage
@@ -66,12 +97,16 @@ const updateCartList = () => {
     const cartProducts = JSON.parse(localStorage.getItem('cartData'));
     let desktopFinalHTML = '';
     let mobileFinalHTML = '';
-    cartProducts.forEach(product => {
+
+    // Desktop
+    cartProducts.forEach((product, i) => {
         const updatedProductHTML = `
         <div class="pro-box">
+        ${i !== 0 ? `
             <span class="details-close-icon product-cross">
                 <i onClick="removeProduct('${product.id}')" style="color: #e5183c;" class="fas fa-times-circle cursor"></i>
-            </span>
+            </span>` : ''} 
+
             <span class="products-ds">
                 <h4>${product.title}</h4>
                 <p>${product.description}</p>
@@ -83,21 +118,23 @@ const updateCartList = () => {
 
         desktopFinalHTML += updatedProductHTML;
 
-        // Update Product - Mobile (Image, title, description & price)
+        // Mobile
         const updatedProductHTML2 = `
+                <div id="exited-product-mobile">
                         <div class="pro-box">
-                            <span class="products-cart-image">
-                                <img id="mobile-pack-image" src=${product.image} class="img-fluid">
-                            </span>
+                            <span class="products-cart-image"> <img id="mobile-pack-image" src=${product.image} class="img-fluid"> </span>
                             <span class="products-ds">
                                 <h4>${product.title}</h4>
-                                <p>${product.description}</p>
+                            <p>${product.description}</p>
                             </span>
 
-                            <span class="details-close-icon">
-                                 <i style="color: #e5183c;" class="fas fa-times-circle cursor"></i>
-                            </span>
+                             ${i !== 0 ? `
+                        <span class="details-close-icon product-cross">
+                            <i onClick="removeProduct('${product.id}')" style="color: #e5183c;" class="fas fa-times-circle cursor"></i>
+                        </span>` : ''} 
                         </div>
+                    </div>
+                <hr />
     `;
 
         mobileFinalHTML += updatedProductHTML2;
@@ -114,24 +151,19 @@ const updateCartList = () => {
 
 // Check if the selected pack exists in localStorage
 if (selectedPack) {
-    console.log("Retrieved cartDat:", cartData);
+    console.log("Retrieved cartData:", cartData);
     console.log("Retrieved selected pack:", selectedPack);
 
     // Elements to dynamically update - Desktop
     const packImage = document.getElementById('pack-image');
-    const cartPrice = document.getElementById('total-cart-price');
 
     // Mobile Elements
     const packImageMobile = document.getElementById('mobile-pack-image');
-    const cartTotalMobile = document.getElementById('mobile-total-cartPrice');
-    const cartPriceMobile = document.querySelector('.mobCartAmount');
+    const cartCount = document.querySelector('.count-value');
 
     // Update "Package image dynamically"
     packImage.src = selectedPack.image;
     packImageMobile.src = selectedPack.image;
-    cartPrice.innerText = `$${selectedPack.price}`;
-    cartPriceMobile.innerText = selectedPack.price;
-    cartTotalMobile.innerText = `$${selectedPack.price}`;
 
     // Update Cart list
     updateCartList();
@@ -139,8 +171,23 @@ if (selectedPack) {
     console.log("No selected pack found in localStorage.");
 };
 
+// Update Total of Cart
+const updateTotal = () => {
+    const cartData = JSON.parse(localStorage.getItem('cartData'));
+    const cartPrice = document.getElementById('total-cart-price');
+    const cartTotalMobile = document.getElementById('mobile-total-cartPrice');
+    const cartPriceMobile = document.querySelector('.mobCartAmount');
+    const totalPrice = cartData.reduce((total, product) => total += Number(product.price), 0);
+    cartPrice.innerText = `$${+totalPrice}`;
+    cartPriceMobile.innerText = `${+totalPrice}`;
+    cartTotalMobile.innerText = `$${+totalPrice}`;
+}
+
 // updateAdditional products
 updateAdditionalHTML();
+
+// Update total of carts ( Desktop & Mobile )
+updateTotal();
 
 // UpdateCartData
 const updateCart = (addProduct) => {
@@ -153,12 +200,15 @@ const updateCart = (addProduct) => {
 
 const removeProduct = (id) => {
     const oldCartData = JSON.parse(localStorage.getItem('cartData'));
+    console.log("oldCartData:", oldCartData);
+
     const updatedCartData = oldCartData.filter(p => p.id !== id);
     localStorage.setItem('cartData', JSON.stringify(updatedCartData));
 
     // Re-render HTML
     updateCartList();
     updateAdditionalHTML();
+    updateTotal();
 };
 
 // To add a new product in the cart
@@ -167,6 +217,7 @@ const addProduct = (id) => {
     updateCart(selectedProduct);
     updateCartList();
     updateAdditionalHTML();
+    updateTotal();
 };
 
 // Event listener for closing the product box when clicking on the close icon
